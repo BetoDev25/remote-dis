eaf16ea9221c7990b5ad53414d3e016d0e596850
Norberto Merani <nmera003@fiu.edu>
2026-07-24 19:09:23 -0400

    second commit

--- cache/cache.go
+++ cache/cache.go
@@ -6,6 +6,12 @@ import (
 	"time"
 )
 
+type Stats struct {
+	Hits      int
+	Misses    int
+	Evictions int
+}
+
 type item struct {
 	value   interface{}
 	expires time.Time
@@ -17,6 +23,7 @@ type Cache struct {
 	mu      sync.Mutex
 	maxsize int
 	order   *list.List
+	stats   Stats
 }
 
 func New(maxsize int) *Cache {
@@ -24,17 +31,25 @@ func New(maxsize int) *Cache {
 		store:   make(map[string]*item),
 		order:   list.New(),
 		maxsize: maxsize,
+		stats: Stats{
+			Hits:      0,
+			Misses:    0,
+			Evictions: 0,
+		},
 	}
 }
 
 func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
+	//Eviction
 	if len(c.store) >= c.maxsize {
 		oldest := c.order.Back()
 		keyRemove := oldest.Value.(string)
 		delete(c.store, keyRemove)
 		c.order.Remove(oldest)
+		c.stats.Evictions += 1
 	}
 
 	elem := c.order.PushFront(key)
@@ -50,15 +65,18 @@ func (c *Cache) Get(key string) (interface{}, bool) {
 	defer c.mu.Unlock()
 	item, ok := c.store[key]
 	if !ok {
+		c.stats.Misses += 1
 		return nil, false
 	}
 
 	if time.Now().After(item.expires) {
 		delete(c.store, key)
+		c.stats.Misses += 1
 		return nil, false
 	}
 
 	c.order.MoveToFront(item.element)
+	c.stats.Hits += 1
 	return item.value, true
 }
 
@@ -70,4 +88,23 @@ func (c *Cache) Delete(key string) {
 		c.order.Remove(it.element)
 		delete(c.store, key)
 	}
+}
+
+func (c *Cache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.store = make(map[string]*item)
+	c.order = list.New()
+	c.stats = Stats{
+		Hits:      0,
+		Misses:    0,
+		Evictions: 0,
+	}
+}
+
+func (c *Cache) Stats() Stats {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.stats
 }
